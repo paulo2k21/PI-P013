425a5c1d560578d9c8646f91a3eb3a29b6db16d6
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-09-15 09:13:55 -0300

    Adicionando atividade para produção

---
+++ empresaOnibus.cpp
@@ -0,0 +1,33 @@
+/*Exercício 1:
+
+Uma empresa de ônibus faz diariamente 5 viagens de ida e 5 viagens de volta entre
+as cidades do Rio de Janeiro e São Paulo. Cada ônibus tem 40 assentos para serem
+preenchidos por passageiros. A empresa costuma vender as passagens
+antecipadamente, e para cada passagem vendida é anotado o número do assento, a
+data e hora, o CPF, o nome e a idade do passageiro. As passagens são vendidas por
+80 reais cada.
+
+Faça um programa para gerenciar a venda de passagens de ônibus. O programa deve
+possibilitar obter as seguintes informações:
+
+1. Qual o total arrecadado para uma determinada viagem.
+2. Qual o total arrecadado em um determinado mês.
+3. Qual o nome do passageiro de uma determinada poltrona P de uma
+determinada viagem.
+4. Qual o horário de viagem mais rentável.
+5. Qual a média de idade dos passageiros.*/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+
+
+
+
+
+
+    return(0);
+}
