7cb9043c87fadae98364dce855c4ffe47a037ae9
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-09-15 09:19:00 -0300

    Adicinando arquivos cpp para produção das atividades

---
+++ empregados.cpp
@@ -0,0 +1,22 @@
+/*Exercício 2:
+Crie um tipo estruturado (struct) empregado para armazenar dados (nome,
+sobrenome, ano de nascimento, RG, ano de admissão, salário) de empregados de
+uma empresa. Defina um vetor de empregados para armazenar até 50 empregados.
+Faça uma função chamada “Reajusta_dez_porcento( )” que receba por parâmetro o
+vetor de empregados e a quantidade de elementos no vetor e atualize o salário de
+cada empregado em 10%. Crie uma função main para testar a função.*/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+
+
+
+
+
+
+    return(0);
+}
---
+++ supermercado.cpp
@@ -0,0 +1,33 @@
+/*Exercício 3:
+Faça um programa para um supermercado para a consulta de preço de produtos. O
+programa deverá armazenar de cada produto o seu código, seu nome e seu preço.
+Ao utilizar o programa o usuário deve poder:
+
+1. Inserir um novo produto
+2. Excluir um produto cadastrado
+3. Listar todos os produtos com seus respectivos códigos e preços
+4. Consultar o preço de um produto através de seu código.
+
+O código deve ser formado de uma string numérica de 13 caracteres
+O nome deve ter um tamanho de no máximo 20 caracteres
+O preço deve ter duas casas decimais
+O sistema deve permitir o cadastro de até 300 produtos diferentes.
+O sistema deve controlar para que não tenha produtos com o mesmo código ou
+mesmo nome, não permitindo o seu cadastro.
+O sistema deverá ser feito modularizados, com qualidade e utilizando estrutura na
+sua implementação.*/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+
+
+
+
+
+
+    return(0);
+}
